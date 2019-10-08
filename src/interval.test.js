4b9ca02e78aad2bd3c7252af747c56c1b35852b8
Frescinel <frescinel.bart@etudiant.univ-rennes1.fr>
2019-10-08 12:04:34 +0200

    Implémentation de la fonction union et des tests

--- src/interval.test.js
+++ src/interval.test.js
@@ -45,3 +45,42 @@ describe('includes', function () {
 
 
 });
+
+describe('union', function () {
+
+    test('Test résultat retourné est un array', () => {
+      var interval1 = new Interval(1,3);
+      var interval2 = new Interval(2,5);
+      expect(Array.isArray(interval1.union(interval2))).toBe(true);
+    });
+
+    test('Test [1,3] union [2,5] equal [[1,5]]', () => {
+        var interval1 = new Interval(1,3);
+        var interval2 = new Interval(2,5);
+        var res = new Interval(1,5);
+        var tab = [res]
+        expect(interval1.union(interval2)).toEqual(tab);
+    });
+
+    test('Test [1,3] union [4,8] equal [[1,3],[4,8]]', () => {
+        var interval1 = new Interval(1,3);
+        var interval2 = new Interval(4,8);
+        var tab = [interval1,interval2]
+        expect(interval1.union(interval2)).toEqual(tab);
+    });
+
+    test('Test [2,9] union [3,5] equal [[2,9]]', () => {
+        var interval1 = new Interval(2,9);
+        var interval2 = new Interval(3,5);
+        var tab1 = [interval1];
+        expect(interval1.union(interval2)).toEqual(tab1);
+    });
+
+    test('Test [4,6] union [2,9] equal [[2,9]]', () => {
+        var interval1 = new Interval(4,6);
+        var interval2 = new Interval(2,9);
+        var tab1 = [interval2];
+        expect(interval1.union(interval2)).toEqual(tab1);
+    });
+
+});
