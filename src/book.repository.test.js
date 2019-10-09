e01e4519bca33b1f514b2f0c7bad88369563a58f
Frescinel <frescinel.bart@etudiant.univ-rennes1.fr>
2019-10-09 23:47:47 +0200

    Implémentations de la fonction getCountBookAddedByMonth et des tests

--- src/book.repository.test.js
+++ src/book.repository.test.js
@@ -103,3 +103,60 @@ describe('Retourne un livre selon le nom', function () {
         expect(repository.getBookByName()).toEqual(book);
     });
 });
+
+describe('Nombre de livre ajouté par mois', function () {
+
+
+  test('Fonction value appelée 1 fois ', () => {
+    let books = [
+  		{name: "hiver",added_at: "2019-02-22"},
+  		{name: "hiver",added_at: "2019-11-03"},
+  		{name: "hiver",added_at: "2019-11-15"}
+  	];
+
+    const dbMock = {
+      get : jest.fn().mockReturnThis(),
+      filter: jest.fn().mockReturnThis(),
+      sortBy: jest.fn().mockReturnThis(),
+      value : jest.fn().mockReturnValue(books)
+    };
+
+    const repository = new BookRepository(dbMock);
+    repository.getCountBookAddedByMonth();
+
+    expect(dbMock.value.mock.calls.length).toBe(1);
+  });
+
+  test('Livre par mois', () => {
+    let books = [
+  		{name: "hiver",added_at: "2019-02-22"},
+  		{name: "hiver",added_at: "2019-11-03"},
+  		{name: "hiver",added_at: "2019-11-15"}
+  	];
+    let result = [
+      {
+        year: 2019,
+        month: 2,
+        count: 1,
+        count_cumulative: 1
+      },
+      {
+        year: 2019,
+        month: 11,
+        count: 2,
+        count_cumulative: 3
+      },
+  	];
+    const dbMock = {
+      get : jest.fn().mockReturnThis(),
+      filter: jest.fn().mockReturnThis(),
+      sortBy: jest.fn().mockReturnThis(),
+      value : jest.fn().mockReturnValue(books)
+    };
+
+    const repository = new BookRepository(dbMock);
+
+    expect(repository.getCountBookAddedByMonth('hiver')).toStrictEqual(result);
+  });
+
+});
