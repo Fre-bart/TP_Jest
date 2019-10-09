a54b90a0526e137a749391f3a7ba5aec52e8c679
Frescinel <frescinel.bart@etudiant.univ-rennes1.fr>
2019-10-09 23:25:37 +0200

    Implémentations fonction getTotalCount et des tests

--- src/book.repository.test.js
+++ src/book.repository.test.js
@@ -15,3 +15,31 @@ describe('Book repository Save', function () {
         expect(dbMock.write.mock.calls.length).toBe(1);
     });
 });
+
+describe('Nombre total de libre', function () {
+
+  test('Fonction value appelée 1 fois', () => {
+
+    const dbMock = {
+        get : jest.fn().mockReturnThis(),
+        size : jest.fn().mockReturnThis(),
+        value : jest.fn().mockReturnValue(0)
+    };
+      const repository = new BookRepository(dbMock);
+      repository.getTotalCount();
+
+      expect(dbMock.value.mock.calls.length).toBe(1);
+  });
+
+    test('Nombre total de libre', () => {
+
+        const dbMock = {
+            get : jest.fn().mockReturnThis(),
+            size : jest.fn().mockReturnThis(),
+            value : jest.fn().mockReturnValue(0)
+        };
+        const repository = new BookRepository(dbMock);
+
+        expect(repository.getTotalCount()).toBe(0);
+    });
+});
